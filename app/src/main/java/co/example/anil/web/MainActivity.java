1c075f6098101cb7905e2aed35c013d4c0ca2778
anil <anil.mohanty01@gmail.com>
2017-08-10 21:49:25 +0530

    Initial commit

---
+++ app/src/main/java/co/example/anil/web/MainActivity.java
@@ -0,0 +1,68 @@
+package co.example.anil.web;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.DownloadListener;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    WebView wv;
+    int per;
+    String per1[]={Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    int req_code=2711;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        per= ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        wv= (WebView) findViewById(R.id.wv1);
+        wv.loadUrl("http://www.ddceutkal.ac.in/");
+        WebViewClient wvc= new WebViewClient();
+        wv.setWebViewClient(wvc);
+
+        WebSettings ws= wv.getSettings();
+        ws.setJavaScriptEnabled(true);
+        if(per== PackageManager.PERMISSION_GRANTED) {
+            wv.setDownloadListener(new DownloadListener() {
+                @Override
+                public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "download");
+                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    dm.enqueue(request);
+                    Toast.makeText(getApplicationContext(), "File is downloading", Toast.LENGTH_LONG).show();
+                }
+            });
+        }else{
+            ActivityCompat.requestPermissions(this,per1,req_code);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()) {
+            wv.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+}
