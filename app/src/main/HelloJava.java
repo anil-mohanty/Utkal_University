5b876d3931aba6f93457ccbaceb43308835d11f0
Ubuntu <anilmohantycgi@anilmohantycgi.f02qmgfvt1kelnxzudytmdxc2b.gx.internal.cloudapp.net>
2020-10-18 09:36:17 +0000

    2nd commit

--- app/src/main/HelloJava.java
+++ app/src/main/HelloJava.java
@@ -2,7 +2,7 @@ class HelloJava {
 
   public static void main (String args []) {
 
-      system.out.println(“I am your Java Program. Thank you! ”)
+      system.out.println(“I am your Java Program. Thank you!Looks cool to me ”);
 
   }
 
